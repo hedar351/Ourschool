241fe0416719a9f8e5ab28a2cc9eaaafbccbe124
hedar351 <darkboyyahoo.com@gmail.com>
2026-06-26 02:40:45 +0300

    kk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,10 @@ plugins {
 
 android {
     namespace = "com.example.ourschool"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    
+    compileSdk = 34 
+    
+    ndkVersion = "28.2.13676358" 
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -20,20 +22,17 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.ourschool"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        
+        minSdk = flutter.minSdkVersion 
+        
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
