43bc8145700f1dde4a68f98416f2a1f80db3cf33
hedar351 <darkboyyahoo.com@gmail.com>
2026-07-17 04:30:57 +0300

    fix

--- lib/features/Counselor/UI/widget/SubjectCard.dart
+++ lib/features/Counselor/UI/widget/SubjectCard.dart
@@ -9,7 +9,7 @@ class SubjectCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
+    // final theme = Theme.of(context);
     return Card(
       elevation: 4,
       margin: const EdgeInsets.only(bottom: 12),
